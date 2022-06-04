26c56dac69e5f069f9a7aee3d98c3880c5cf6c36
Samuel Tegsten <samuel.tegsten@gmail.com>
2022-06-04 12:48:54 +0200

    Hello World

--- JavaBasics/A_HelloWorld.java
+++ JavaBasics/A_HelloWorld.java
@@ -1,6 +1,6 @@
 package LearnJava.JavaBasics;
 
-public class HelloWorld {
+public class A_HelloWorld {
     static public void main(String args[])
     {
         System.out.println("Hello World");
